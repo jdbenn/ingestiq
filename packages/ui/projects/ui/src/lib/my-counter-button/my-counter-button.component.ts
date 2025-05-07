55b86e5f93fc96f3bad2f6a093716ff65b9214a2
John Bennett <john.bennett@prismhr.com>
2025-05-07 14:00:20 -0500

    upgrade packages to v19

--- packages/ui/projects/ui/src/lib/my-counter-button/my-counter-button.component.ts
+++ packages/ui/projects/ui/src/lib/my-counter-button/my-counter-button.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'lib-my-counter-button',
-  standalone: true,
-  imports: [],
-  templateUrl: './my-counter-button.component.html',
-  styleUrl: './my-counter-button.component.css'
+    selector: 'lib-my-counter-button',
+    imports: [],
+    templateUrl: './my-counter-button.component.html',
+    styleUrl: './my-counter-button.component.css'
 })
 export class MyCounterButtonComponent {
   count = 0;
