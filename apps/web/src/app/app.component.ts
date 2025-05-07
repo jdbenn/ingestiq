ca38230af65eb108ec395a9b02033b852df62a84
John Bennett <john.bennett@prismhr.com>
2025-05-07 14:18:40 -0500

    rename ui > shared

--- apps/web/src/app/app.component.ts
+++ apps/web/src/app/app.component.ts
@@ -2,11 +2,10 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MyCounterButtonComponent } from '@repo/ui';
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, MyCounterButtonComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    imports: [RouterOutlet, MyCounterButtonComponent],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'web';
